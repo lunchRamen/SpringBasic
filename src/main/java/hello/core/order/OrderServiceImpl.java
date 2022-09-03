f3cef010a5bc54db793227cd31665b3dafd1856a
lunchRamen <bloom6561@naver.com>
2022-09-04 05:43:59 +0900

    Fifth Commit about SpringBasic Learn about @Autowired , lombok , @Qualifier, @Primary , Custom Annotation , manual wired case.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,28 +1,32 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor 롬복의 생성자 자동생성 어노테이션
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
+    // cmd + opt + b = 해당 클래스로 혹은 인터페이스라면 구현체로 이동 가능하게 해줌.
     private final DiscountPolicy discountPolicy;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    @Autowired    //생성자가 1개인 경우는 생략이 가능하다.
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    } //롬복의 어노테이션으로, 생성자 생성을 대체한다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
